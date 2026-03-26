be81c3bcd8951ecc86e9eb4c5283affb7c9dcbf1
eueo8259 <eueo8259@gmail.com>
2026-03-26 15:05:49 +0900

    feat: jmh 실험

---
+++ src/jmh/java/benchmark/CourseBenchmark.java
@@ -0,0 +1,38 @@
+package benchmark;
+
+import org.example.domain.Course;
+import org.example.domain.ReentrantLockCourse;
+import org.example.domain.SynchronizedCourse;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Fork(1)
+@Warmup(iterations = 2, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Threads(32) // 32개의 스레드가 지속적으로 경합 발생
+public class CourseBenchmark {
+
+    private Course syncCourse;
+    private Course lockCourse;
+
+    @Setup(Level.Iteration)
+    public void setup() {
+        // 매 Iteration 시작 시 객체 초기화 (정원 0명으로 리셋)
+        syncCourse = new SynchronizedCourse();
+        lockCourse = new ReentrantLockCourse();
+    }
+
+    @Benchmark
+    public void measureSynchronized() {
+        syncCourse.enroll();
+    }
+
+    @Benchmark
+    public void measureReentrantLock() {
+        lockCourse.enroll();
+    }
+}
